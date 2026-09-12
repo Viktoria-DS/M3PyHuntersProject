c7aaabe93f85a0f9b15881fa17a023f56b2a7f36
Viktoria <viktoriia.vorobyeva@fmail.com>
2026-09-12 08:07:02 +0200

    add product detail view

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from products.apps import ProductsConfig
-from products.views import ProductListView, GuidesRecipesView
+from products.views import ProductListView, GuidesRecipesView, ProductDetailView
 from django.urls import path
 
 app_name = ProductsConfig.name
@@ -7,4 +7,5 @@ app_name = ProductsConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('guides-recipes/', GuidesRecipesView.as_view(), name='guides_recipes'),
+    path('<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, DetailView
 
 from products.models import Product
 
@@ -12,3 +12,12 @@ class ProductListView(ListView):
 
 class GuidesRecipesView(TemplateView):
     template_name = 'products/guides_recipes.html'
+
+class ProductDetailView(DetailView):
+    template_name = 'products/product_detail.html'
+    queryset = Product.objects.all().select_related('category').prefetch_related('reviews')
+    def get_context_data(self, **kwargs):
+        product = Product.objects.get(slug=self.kwargs['slug'])
+        context = super().get_context_data(**kwargs)
+        context['product'] = product
+        return context
